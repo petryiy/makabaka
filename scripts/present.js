5d3d9c74f4240b0bcaccbaebf6af0b0328f74d3c
petryiy <0273039323m@gmail.com>
2025-07-10 15:30:57 +1000

    changed the style of present

--- scripts/present.js
+++ scripts/present.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
         "既然命运充满不确定性",
         "不如现在就去做想做的事，见想见的人",
         "漂流瓶终将上岸",
-        "而当你看到这的时候，我应该到你家楼下了"
+        "而当你看到这的时候，我应该到你家楼下了",
+        "下来吧 迎接你的第三个礼物"
     ];
 
     lines.forEach((line, index) => {
@@ -16,7 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('line1'),
             document.getElementById('line2'),
             document.getElementById('line3'),
-            document.getElementById('line4')
+            document.getElementById('line4'),
+            document.getElementById('line5')
         ];
 
         elements.forEach((el, index) => {
@@ -30,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         document.getElementById('home-btn').style.transform = 'translateY(0)';
                     }, 1000);
                 }
-            }, index * 1500);
+            }, index * 2500);
         });
     }
     setTimeout(fadeInElements, 500);
